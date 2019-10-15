face5f3ee5f38a33d4d3e600e72cb5512cf671aa
Calvin Chu <calvinchu@ecoworkinc.com>
2019-10-15 20:03:02 +0800

    Modify Mandarin profile content.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,7 @@
 class ProfilesController < ApplicationController
 
+  before_action :common_params
+
   def en
     @nav_title = 'English Profile'
   end
@@ -7,5 +9,15 @@ class ProfilesController < ApplicationController
   def tw
     @nav_title = '中文履歷'
   end
+
+  private
+
+  def common_params
+    @proficient = ['Ruby', 'Ruby on Rails', 'Ruby on Jets', 'HAML', 'SCSS', 'Redis', 'Docker',
+      'Docker-Compose', 'Bootstrap', 'Git', 'Vagrant', 'Travis-CI', 'Gitlab-CI', 'Cucumber', 'Rspec']
+    @familier = ['Javascript', 'CoffeeScript', 'jQuery', 'AngularJS', 'NodeJS', 'Mocha', 'Serverless',
+      'C', 'SQL', 'HTML/CSS', 'Bash/Shell Script', 'Chef', 'AWS']
+  end
+
 
 end
